1bf17cfde6213b817fc561c35397cc5c92892cde
Paweł Piekutowski <futureiscode@gmail.com>
2019-05-13 14:41:48 +0200

    Dodanie zmiennych oraz pętli

--- Lekcja6/Program.cs
+++ Lekcja6/Program.cs
@@ -10,6 +10,31 @@ namespace Lekcja6
     {
         static void Main(string[] args)
         {
+            //Wyświetl w konsoli prostokąt o szerokości x oraz wysokości y
+            //x oraz y podaje użytkownik, prostokąt zbudowany jest z "*"
+
+            Console.Write("Podaj szerokość: ");
+            string xs = Console.ReadLine();
+            Console.Write("Podaj wysokość");
+            string ys = Console.ReadLine();
+
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j <x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(x+" "+y);
+
+            Console.ReadKey();
+
+
         }
     }
 }
